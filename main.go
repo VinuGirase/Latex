af305b2e515e3b679edc817b6441f794528c523e
Vinu Girase <114801691+VinuGirase@users.noreply.github.com>
2025-02-17 19:35:26 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,83 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
-func main() {
-	// LaTeX content
-	latexCode := `
-    \documentclass[a4paper,10pt]{article}
-\usepackage[utf8]{inputenc}
-\usepackage{titlesec}
-\usepackage{hyperref}
-\usepackage{xcolor}
-\usepackage{multicol}
-\usepackage{parskip}
-
-% Manual margin adjustments
-\setlength{\topmargin}{-0.5in}
-\setlength{\evensidemargin}{0in}
-\setlength{\oddsidemargin}{0in}
-\setlength{\textwidth}{6.5in}
-\setlength{\textheight}{9in}
-\setlength{\headheight}{0in}
-\setlength{\headsep}{0.25in}
-\setlength{\footskip}{0.5in}
-\setlength{\parskip}{1em}
-
-% Section formatting
-\titleformat{\section}{\Large\bfseries}{}{0em}{}[\titlerule]
-\titleformat{\subsection}{\large\bfseries}{}{0em}{}
-
-% Hyperlink settings
-\hypersetup{
-    colorlinks=true,
-    linkcolor=black,
-    filecolor=black,      
-    urlcolor=black,
-}
-
-% Custom commands
-\newcommand{\resumeItem}[2]{
-  \item\textbf{#1}{: #2}
-}
-\newcommand{\resumeSubheading}[4]{
-  \item
-    \textbf{#1} \hfill #2 \\
-    \textit{#3} \hfill \textit{#4}
-}
-\newcommand{\resumeSubSubheading}[2]{
-    \item
-    \textbf{#1} \hfill #2
-}
-
-\begin{document}
-
-\begin{tabbing}
-\hspace{4in} \= \kill
-{\Huge \textbf{Ritesh Girase}} \\
-\href{mailto:riteshgirasecode@gmail.com}{riteshgirasecode@gmail.com} \> 
-\hspace{1.8in}
-\href{https://www.linkedin.com/}{LinkedIn} \\
-Nashik, Maharashtra, India \> \hspace{1.85in}\href{https://github.com}{GitHub} \\ 
-9552566981 \\
-\end{tabbing}
-\vspace{-0.4cm}
-
-\end{document}
-
-
-	`
-
-	// Write LaTeX code to a .tex file
-	texFile := "document.tex"
-	err := os.WriteFile(texFile, []byte(latexCode), 0644)
-	if err != nil {
-		fmt.Println("Error writing LaTeX file:", err)
+// Function to handle the /get-pdf API route
+func getPDFHandler(w http.ResponseWriter, r *http.Request) {
+	// Only accept POST requests
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
+
+	// Read the LaTeX code from the request body
+	latexCode, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading LaTeX code", http.StatusInternalServerError)
+		return
+	}
+
+	// Write LaTeX code to a temporary .tex file
+	texFile := fmt.Sprintf("document_%d.tex", time.Now().Unix())
+	err = ioutil.WriteFile(texFile, latexCode, 0644)
+	if err != nil {
+		http.Error(w, "Error writing LaTeX file", http.StatusInternalServerError)
+		return
+	}
+	defer os.Remove(texFile)
 
 	// Execute pdflatex command to generate the PDF
 	cmd := exec.Command("pdflatex", texFile)
@@ -86,9 +39,34 @@ Nashik, Maharashtra, India \> \hspace{1.85in}\href{https://github.com}{GitHub} \
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println("Error compiling LaTeX:", err)
+		http.Error(w, "Error compiling LaTeX", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Println("PDF generated successfully!")
+	// Serve the generated PDF file
+	pdfFile := fmt.Sprintf("document_%d.pdf", time.Now().Unix())
+	defer os.Remove(pdfFile)
+
+	// Move the generated PDF to the correct file
+	err = os.Rename("document.pdf", pdfFile)
+	if err != nil {
+		http.Error(w, "Error moving PDF file", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the headers to prompt for file download
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", pdfFile))
+	w.Header().Set("Content-Type", "application/pdf")
+	http.ServeFile(w, r, pdfFile)
+}
+
+func main() {
+	// Set up the API route
+	http.HandleFunc("/get-pdf", getPDFHandler)
+
+	// Start the HTTP server
+	fmt.Println("Starting server on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
